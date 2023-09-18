f7cdd6584127486c20e782ea0699131d4b34ccc7
DavidGonyAtos <36034760+DavidGonyAtos@users.noreply.github.com>
2023-09-18 10:25:46 +0200

    Add files via upload

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,22 @@
+using System.Data.SqlClient.SqlConnection;
+using System.Data.SqlClient.SqlCommand;
+using System.Data.SqlClient.SqlDataReader;
+
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // using System.Data.SqlClient.SqlConnection
+            // using System.Data.SqlClient.SqlCommand
+            // using System.Data.SqlClient.SqlDataReader
+        }
+    }
+}
